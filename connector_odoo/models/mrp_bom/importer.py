2ffd43cb8dd6d030e8f883fb79f3d030fe244b83
yiğit budak <yibudak@gmail.com>
2023-03-30 18:05:54 +0300

    [16.0][FIX] Populate force on mrp models

--- connector_odoo/models/mrp_bom/importer.py
+++ connector_odoo/models/mrp_bom/importer.py
@@ -30,7 +30,7 @@ class MrpBomBatchImporter(Component):
         )
         for external_id in external_ids:
             job_options = {"priority": 15}
-            self._import_record(external_id, job_options=job_options)
+            self._import_record(external_id, job_options=job_options, force=force)
 
 
 class MrpBomMapper(Component):
